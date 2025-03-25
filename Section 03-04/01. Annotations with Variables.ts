8dd309c6a546edfeaf9289f4200132cc657f8f13
winsonallan <winsonallanw@gmail.com>
2025-03-25 10:20:06 +0700

    biome js

--- Section 03-04/01. Annotations with Variables.ts
+++ Section 03-04/01. Annotations with Variables.ts
@@ -1,35 +1,35 @@
-let apples: number = 5;
-let apples2 = 5;
+const apples: number = 5;
+const apples2 = 5;
 
-let speed: string = "fast";
+const speed: string = "fast";
 // let speed: string = 10; //error caught
 
-let hasName: boolean = true;
-let hasName2 = true;
+const hasName: boolean = true;
+const hasName2 = true;
 
-let nothingMuch: null = null;
-let nothing: undefined = undefined;
+const nothingMuch: null = null;
+const nothing: undefined = undefined;
 
-let now: Date = new Date();
+const now: Date = new Date();
 
 // Arrays
-let colors: string[] = ["red", "green", "blue"];
-let myNumbers: number[] = [1, 2, 3];
-let truths: boolean[] = [true, true, false];
+const colors: string[] = ["red", "green", "blue"];
+const myNumbers: number[] = [1, 2, 3];
+const truths: boolean[] = [true, true, false];
 
 class Car {}
 
-let car: Car = new Car();
+const car: Car = new Car();
 
 // Object literal
-let point: { x: number; y: number } = {
-  x: 10,
-  y: 20,
+const point: { x: number; y: number } = {
+	x: 10,
+	y: 20,
 };
 
 // Function
 const logNumber: (i: number) => void = (i: number) => {
-  console.log(i);
+	console.log(i);
 };
 
 // Section 3
@@ -42,21 +42,21 @@ const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
 
 // 2) When we declare a variable on one line and initialize it later
-let words = ["red", "green", "blue"];
+const words = ["red", "green", "blue"];
 let foundWord: boolean;
 
 for (let i = 0; i < words.length; i++) {
-  if (words[i] === "green") {
-    foundWord = true;
-  }
+	if (words[i] === "green") {
+		foundWord = true;
+	}
 }
 
 // 3) Variable whose type cannot be inferred correctly
-let numbers = [-10, -1, 12];
+const numbers = [-10, -1, 12];
 let numberAboveZero: boolean | number = false;
 
 for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] > 0) {
-    numberAboveZero = numbers[i];
-  }
+	if (numbers[i] > 0) {
+		numberAboveZero = numbers[i];
+	}
 }
