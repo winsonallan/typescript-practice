b34085a6f3a20e78221d6e440e6048d0c249d9d1
winsonallan <winsonallanw@gmail.com>
2025-03-26 09:32:52 +0700

    section 3 & 4 done

--- Section 03-04/02. Type Annotations in Functions.ts
+++ Section 03-04/02. Type Annotations in Functions.ts
@@ -1,7 +1,39 @@
+// Parameters don't get type inference. Returns get type inference
 const add = (a: number, b: number): number => {
 	return a + b;
 };
 
-function testing(test: number): void {
-	console.log(`Test ${test}`);
+const subtract = (a: number, b: number) => {
+	a - b;
+};
+
+function divide(a: number, b: number): number {
+	return a / b;
 }
+
+const multiply = (a: number, b: number): number => a * b;
+
+const logger = (message: string): void => {
+	console.log(message);
+};
+
+const throwError = (message: string): never => {
+	throw new Error(message);
+};
+
+const todaysWeather = {
+	date: new Date(),
+	weather: "sunny",
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void =>
+	console.log(forecast.date, forecast.weather);
+
+logWeather(todaysWeather);
+
+const logWeather2 = ({
+	date,
+	weather,
+}: { date: Date; weather: string }): void => console.log(date, weather);
+
+logWeather2(todaysWeather);
