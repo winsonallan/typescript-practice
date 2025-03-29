978fb211227d71d8999ddbe8c025944f39ada9bd
winsonallan <winsonallanw@gmail.com>
2025-03-29 18:48:31 +0700

    tsconfig and npms

---
+++ Section 10 - More on Design Patterns/src/index.ts
@@ -0,0 +1,33 @@
+class Sorter {
+  collection: number[] | string;
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }
+
+  sort(): void {
+    const {length} = this.collection;
+
+    for (let i =0; i<length; i++) {
+      for (let j = 0; j< length-i-1; j++) {
+        // If collection is an array of numbers
+        if (this.collection instanceof Array){
+          if (this.collection[j] > this.collection[j+1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j+1];
+            this.collection[j+1] = leftHand;
+          }
+        }
+
+        // If collection is a string
+        if (typeof this.collection === 'string') {
+          
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
