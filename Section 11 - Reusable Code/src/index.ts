2ede71ffd82d5a621ac189fded845a3c5829e5aa
winsonallan <winsonallanw@gmail.com>
2025-04-01 16:51:05 +0700

    npm node, index ts, for loop  logic

--- Section 11 - Reusable Code/src/index.ts
+++ Section 11 - Reusable Code/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hi There");
+import { CsvFileReader } from "./CsvFileReader";
+
+const reader = new CsvFileReader("football.csv");
+reader.read();
+
+console.log(reader.data);
+
+// enum - enumeration
+enum MatchResult {
+	HomeWin = "H",
+	AwayWin = "A",
+	Draw = "D",
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+	if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+		manUnitedWins++;
+	} else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+		manUnitedWins++;
+	}
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
